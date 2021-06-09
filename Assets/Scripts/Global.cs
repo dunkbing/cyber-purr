b4ce4137c654fad9e59dd2ff5ed23e47155177a2
binh.buidang <dangbinh4869@gmail.com>
2021-06-09 17:04:04 +0700

    [ADD] scripts for spawning/moving helicopters

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    public GameObject helicopterPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnHelicopter), 3f, 3f);
+    }
+
+    private void SpawnHelicopter()
+    {
+        var rightSide = Random.Range(0, 2) == 0;
+        var randomPos = rightSide ? new Vector3(-11, Random.Range(1, 4)) : new Vector3(11, Random.Range(1, 4));
+        var helicopter = Instantiate(helicopterPrefab, randomPos, Quaternion.identity).GetComponent<Helicopter>();
+        Debug.Log($"global {rightSide}");
+        helicopter.RightSide = rightSide;
+    }
+}
---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    public int speed = 10;
+
+    public bool RightSide { get; set; }
+
+    private void Start()
+    {
+        if (!RightSide)
+        {
+            gameObject.transform.Rotate(new Vector3(0, 180, 0));
+            speed *= -1;
+        }
+    }
+
+    private void Update()
+    {
+        if (RightSide)
+        {
+            gameObject.transform.Translate(Vector3.right * (Time.deltaTime * speed));
+            return;
+        }
+        gameObject.transform.Translate(Vector3.left * (Time.deltaTime * speed));
+    }
+}
