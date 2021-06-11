f5bfa9b7a055ce8c9246ff79787e444d5cc95e40
binh.buidang <dangbinh4869@gmail.com>
2021-06-11 14:57:08 +0700

    delete: logging

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -1,11 +1,18 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Helicopter : MonoBehaviour
 {
-    public int speed = 10;
+    private int _speed;
 
     public bool RightSide { get; set; }
     private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _speed = Random.Range(6, 12);
+    }
 
     private void Start()
     {
@@ -16,14 +23,14 @@ public class Helicopter : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (RightSide)
         {
-            _rb.MovePosition(Vector3.right * (Time.deltaTime * speed) + transform.position);
+            _rb.MovePosition(Vector3.right * (Time.fixedDeltaTime * _speed) + transform.position);
             return;
         }
-        _rb.MovePosition(Vector3.left * (Time.deltaTime * speed) + transform.position);
+        _rb.MovePosition(Vector3.left * (Time.fixedDeltaTime * _speed) + transform.position);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
