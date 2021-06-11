b066ccd0e612a820feed76954fb2122eac8b8abd
binh.buidang <dangbinh4869@gmail.com>
2021-06-11 13:14:08 +0700

    [ADD] shooting script

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Camera cam;
+    private GameObject _currentBullet;
+    private readonly Vector3 _spawnPos = new Vector3(0, -3.26f, 0);
+    private const float BulletForce = 10f;
+
+    private Vector3 _mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentBullet = Instantiate(bulletPrefab, _spawnPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        // handle shooting
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private float angl;
+    private void FixedUpdate()
+    {
+        var lookDir = _mousePos - gameObject.transform.position;
+        var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        angl = angle;
+        if (angle < -90 && angle > -180)
+        {
+            angle = -90;
+        } else if (angle > -270 && angle < -180)
+        {
+            angle = -270;
+        }
+        _currentBullet.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void Shoot()
+    {
+        Rigidbody2D rb = _currentBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(_currentBullet.transform.up * BulletForce, ForceMode2D.Impulse);
+        _currentBullet = Instantiate(bulletPrefab, _spawnPos, Quaternion.identity);
+        Debug.Log(angl);
+    }
+
+}
